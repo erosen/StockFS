a4174e2afede861ff72f6f496527e96e9ef2266e
Elie Rosen <mylifeiscomputers@gmail.com>
2012-04-08 19:33:18 -0400

    socket set up function, some symbol input

--- stockfs.c
+++ stockfs.c
@@ -5,8 +5,8 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-int main(void)
-{
+int socket_set_up() {
+	
 	int sd;
 	struct sockaddr_in server;
 
@@ -14,17 +14,26 @@ int main(void)
 
 	memset(&server, 0, sizeof(server));
 	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr("76.13.114.90");
+	server.sin_addr.s_addr = inet_addr("76.13.114.90"); /* download.finance.yahoo.com */
 	server.sin_port = htons(80);
 
 	int result = connect(sd, (struct sockaddr *) &server, sizeof(server));
-	printf("result: %i\n", result);
-	char buffer[1024]= "0";
-	int bytes;
+	
+	return sd;
+}
+	 
+	
 
-	char request[1024] = "GET ";
-	strcat(request, "/d/quotes.csv?s=aapl&f=l1 HTTP/1.0\n");
-	strcat(request, "HOST: download.finance.yahoo.com\n\n");
+int main(void) {
+
+	int bytes, sd = socket_set_up();
+	char request[1024], buffer[1024]; 
+	char * symbol;
+	
+	symbol = "GOOG+AAPL";
+	strcat(request, "GET /d/quotes.csv?s=");
+	strcat(request, symbol);				
+	strcat(request, "&f=snl1c1bab6a5 HTTP/1.0\nHOST: download.finance.yahoo.com\n\n");
 	bytes = send(sd, request, strlen(request), 0);
 	printf("Send %d bytes...\n", bytes);
 	bytes = recv(sd, buffer, 1023, 0);
