bdddd26de0f9b45cb044a2736e54378bed75c4fe
Elie Rosen <mylifeiscomputers@gmail.com>
2012-04-09 19:19:39 -0400

    base FUSE code added

--- stockfs.c
+++ stockfs.c
@@ -1,9 +1,13 @@
+#define FUSE_USE_VERSION  26
+#include <fuse.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <string.h>	/* for memcpy */
 #include <netinet/in.h>
 #include <netdb.h>
+#include <errno.h>
+#include <fcntl.h>
 
 /* This function sets up the socket connection and returns the socket for later use */
 int socket_set_up() {
@@ -22,6 +26,7 @@ int socket_set_up() {
 	
 	return sd;
 }
+
 /* Take in the socket and requested symbol, then output the data the server responds with */
 void getStockInfo(int sd, char* symbol, char *buffer) {
 	
@@ -148,7 +153,94 @@ void parseStockInfo(char *buffer) {
 	printf("Ask Size: %s\n", data[7]);
 }
 
-int main(void) {
+static const char *stockfs_str = "stockfs World!\n";
+static const char *stockfs_path = "/stockfs";
+
+static int stockfs_getattr(const char *path, struct stat *stbuf) {
+    int res = 0;
+
+    memset(stbuf, 0, sizeof(struct stat));
+    if(strcmp(path, "/") == 0) {
+        stbuf->st_mode = S_IFDIR | 0755;
+        stbuf->st_nlink = 2;
+    }
+    else if(strcmp(path, stockfs_path) == 0) {
+        stbuf->st_mode = S_IFREG | 0444;
+        stbuf->st_nlink = 1;
+        stbuf->st_size = strlen(stockfs_str);
+    }
+    else
+        res = -ENOENT;
+
+    return res;
+}
+
+static int stockfs_readdir(const char *path, void *buf, 
+	fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+		
+    (void) offset;
+    (void) fi;
+
+    if(strcmp(path, "/") != 0)
+        return -ENOENT;
+
+    filler(buf, ".", NULL, 0);
+    filler(buf, "..", NULL, 0);
+    filler(buf, stockfs_path + 1, NULL, 0);
+
+    return 0;
+}
+
+static int stockfs_open(const char *path, struct fuse_file_info *fi) {
+    if(strcmp(path, stockfs_path) != 0)
+        return -ENOENT;
+
+    if((fi->flags & 3) != O_RDONLY)
+        return -EACCES;
+
+    return 0;
+}
+
+/*static int stockfs_close(const char *path, struct fuse_file_info *fi) {
+	
+	
+	
+	return 0;
+} */
+
+static int stockfs_read(const char *path, char *buf, 
+	size_t size, off_t offset, struct fuse_file_info *fi) {
+    size_t len;
+    (void) fi;
+    if(strcmp(path, stockfs_path) != 0)
+        return -ENOENT;
+
+    len = strlen(stockfs_str);
+    if (offset < len) {
+        if (offset + size > len)
+            size = len - offset;
+        memcpy(buf, stockfs_str + offset, size);
+    } else
+        size = 0;
+
+    return size;
+}
+
+/*static int stockfs_creat() {
+	return 0;
+} */
+
+static struct fuse_operations stockfs_oper = {
+    .getattr	= stockfs_getattr,
+    .readdir	= stockfs_readdir,
+    .open	= stockfs_open,
+//    .close	= stockfs_close,
+    .read	= stockfs_read,
+   // .creat	= stockfs_creat,
+    
+};
+
+int main(int argc, char *argv[]) {
 
 	int sd = socket_set_up();
 	char *symbol; 
@@ -161,6 +253,6 @@ int main(void) {
 	
 	parseStockInfo(buffer);
 	
-	return 0;
+	return fuse_main(argc, argv, &stockfs_oper, NULL);;
 	
 }
